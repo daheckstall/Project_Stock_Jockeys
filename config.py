119d02da37a05f6f4b03eb53baaf6b7f116c920c
Drew <dr3w.lopresti@gmail.com>
2018-06-27 21:09:42 -0400

    Earnings_Tweets.py.ipynb

---
+++ config.py
@@ -0,0 +1,7 @@
+
+consumer_key = 'XtoC0T5jCTjFDc6eohXaGdaqq' 
+consumer_secret= '82D4izRHuV4kXsCjbybXMQ5DOp5YqKhAmWb9kadSbsu0caX1VX'
+
+access_token = '726806235580882944-CXMj4TyXtcxf1DSmzhVltUr70uBdygF'
+
+access_token_secret = 'i2usdmIqfEh31Lvk8XWWth75gNJ81L5ZTAfsfreDpvqnm'
